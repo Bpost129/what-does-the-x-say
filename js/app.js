b8dc2a5ca152f841ec75343aa974478e2d758a70
Bpost129 <Bpost129@yahoo.com>
2025-12-02 23:18:24 -0500

    initialize variable for fox audio, add event listener to play audio on click

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
+const foxSays = new Audio('../audio/fox.mp3')
+
 const foxImg = document.querySelector('#fox')
+
+foxImg.addEventListener('click', (e) => {
+  foxSays.volume = .04
+  foxSays.play()
+})
