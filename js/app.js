df00e4d204bfbaf47911afd8a3f1a8adc94918e4
Bpost129 <Bpost129@yahoo.com>
2025-12-02 23:28:02 -0500

    add element reference for not-fox div

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const foxSays = new Audio('../audio/fox.mp3')
 
 const foxImg = document.querySelector('#fox')
+const notFoxDiv = document.querySelector('#not-fox')
 
 foxImg.addEventListener('click', (e) => {
   foxSays.volume = .04
